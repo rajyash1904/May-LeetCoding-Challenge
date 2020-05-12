6f8db7b82f24c17cf8067181e33435e3691927ce
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-05-12 14:06:43 +0530

    Update Day12. SingleElementInASortedArray.cpp

--- Day12. SingleElementInASortedArray.cpp
+++ Day12. SingleElementInASortedArray.cpp
@@ -31,10 +31,10 @@ element have first occurrence at odd index and next occurrence at even index.*/
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
         int i=0;
         int j=nums.size()-1;
-        while(i<=j){
-            if(i==j) return nums[i];
+        while(i<j){
             int mid=(j-i)/2 + i;
             if(mid%2==0){
                 if(nums[mid]==nums[mid+1])  i=mid+2;
@@ -44,6 +44,6 @@ public:
                 else j=mid-1;
             }
         }
-        return -1;
+        return nums[i];
     }
 };
